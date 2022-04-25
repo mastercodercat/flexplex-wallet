e736bca3c196af7af251dee5268a8a771130f69f
Arunavo Ray <rayanup3@gmail.com>
2022-04-25 22:39:37 +0530

    ✏️ Typo Fix

--- renderer/src/pages/details.tsx
+++ renderer/src/pages/details.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import NavBar from '../components/NavBar';
-import CollectionDetailsView from '../views/collectionDEtailsView';
+import CollectionDetailsView from '../views/collectionDetailsView';
 
 
 const navigation = [
